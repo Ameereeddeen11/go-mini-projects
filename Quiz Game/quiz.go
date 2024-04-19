5f85aaf63dcecd1b67b4394a0e5e077e445662f8
Ameereeddeen11 <89810178+Ameereeddeen11@users.noreply.github.com>
2024-04-20 00:19:27 +0200

    add score calucation

--- Quiz Game/quiz.go
+++ Quiz Game/quiz.go
@@ -5,13 +5,14 @@ import (
 )
 
 func main() {
+	questions := 3
 	fmt.Println("Welcome to the Quiz Game!")
 	fmt.Println("What is your name?")
 	var name string
 	fmt.Scanln(&name)
 	fmt.Println("Hello", name, "!")
 	fmt.Println("How old are you?")
-	var age int
+	var age uint
 	fmt.Scanln(&age)
 	if age < 18 {
 		fmt.Println("Your are to young to play this game. Goodbye!")
@@ -28,15 +29,34 @@ func main() {
 	fmt.Println("What is the capital of France?")
 	var answer1 string
 	fmt.Scanln(&answer1)
-	if answer1 == "Paris" {
+	if answer1 == "Paris" || answer1 == "paris" {
 		score++
 		fmt.Println("Correct! Your score is ", score)
 	} else {
 		fmt.Println("Incorrect! Your score is ", score)
 	}
 
-}
+	fmt.Println("What is 12 - 5?")
+	var answer2 int
+	fmt.Scan(&answer2)
+	if answer2 == 7 {
+		score++
+		fmt.Println("Correct! Your score is ", score)
+	} else {
+		fmt.Println("Incorrect! Your score is ", score)
+	}
 
-func validation(answer string) {
+	fmt.Println("What is 5 * 5?")
+	var answer3 int
+	fmt.Scan(&answer3)
+	if answer3 == 25 {
+		score++
+		fmt.Println("Correct! Your score is ", score)
+	} else {
+		fmt.Println("Incorrect! Your score is ", score)
+	}
 
+	fmt.Printf("You scored %d out of 1\n", score)
+	percent := (float64(score) / float64(questions)) * 100
+	fmt.Printf("You scored %v%%", percent)
 }
